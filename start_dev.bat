002a5be29dad5405d5cb1ecb16dac03af9a55111
Madhuri Chitikela <madhurisravan.chitikela@gmail.com>
2020-09-20 22:44:39 +0530

    Adding files

--- start_dev.bat
+++ start_dev.bat
@@ -1,4 +1,9 @@
-set FLASK_APP=app.py
-set FLASK_ENV=development
+call python -m venv env
+call env/Scripts/activate
 
-flask run --port 8085
+call pip install -r requirements.txt
+
+call set FLASK_APP=app/run.py
+call set FLASK_ENV=development
+
+call flask run --port 8085 
